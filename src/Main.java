08e3ad492c0ad5c25f901f12841bf118a81ee9ba
ayakend <ayakend@gmail.com>
2022-11-16 08:40:51 +0300

    final

--- src/Main.java
+++ src/Main.java
@@ -10,14 +10,14 @@ public class Main {
             Scanner scanner = new Scanner(System.in);
             System.out.println("Введите фразу: ");
             A.add(scanner.nextLine());
-            System.out.println("Введенная фраза: " + A.get(i) +"| A: " + A);
+            System.out.println("Введенная фраза: " + A.get(i) +"/ A: " + A);
         }
         ArrayList<String> B = new ArrayList<>();
         for (int i = 0; i < 5; i++) {
             Scanner scanner = new Scanner(System.in);
             System.out.println("Введите фразу: ");
             B.add(scanner.nextLine());
-            System.out.println("Введенная фраза: " + B.get(i) +"| B: " + B);
+            System.out.println("Введенная фраза: " + B.get(i) +"/ B: " + B);
         }
         ArrayList<String> C = new ArrayList<>();
         int j = 5;
